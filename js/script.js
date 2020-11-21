e5c61f60be556c0d4131d92dc4f513d1e17e7028
shiyada <anu.ktprm@gmail.com>
2020-11-21 14:45:52 +0530

    meta add

--- js/script.js
+++ js/script.js
@@ -1,18 +1,14 @@
 /**
- * @author Vinit Shahdeo <vinitshahdeo@gmail.com>
+ * 
  */
 (function ($) {
     "use strict";
       $('.sakura-falling').sakura();
 })(jQuery);
 
-$(document).on('click', function(){
-    document.getElementById("my_audio").play();
-    console.log('Shaadi me zaroor aana');
-});
 
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 21, 2020 00:00:00").getTime();
+var countDownDate = new Date("Dec 21git , 2020 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -70,12 +66,3 @@ var styles2 = [
     , 'font-weight: bold'
     , 'font-size: 32px'
 ].join(';');
-
-console.log('\n\n%c SAVE THE DATE: 29th Nov, 2020!', styles);
-
-console.log('%cYour presence is requested!%c\n\nRegards: Vinit Shahdeo', styles1, styles2);
-
-console.log(
-    `%cShaadi me zaroor aana!\n\n`,
-    'color: yellow; background:tomato; font-size: 24pt; font-weight: bold',
-)
